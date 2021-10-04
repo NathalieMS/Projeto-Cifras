ff8df646d7f8c35229f42bb4f1c2fe6dfb702374
NathalieMS <nathalie.moreira@hotmail.com>
2021-10-04 05:27:37 -0300

    Finalizado sem estilização

--- codigo.js
+++ codigo.js
@@ -4,19 +4,22 @@ const cifra = document.getElementById('seleciona');
 const cesar = document.getElementById('cesar');
 const base64 = document.getElementById('base64');
 
+
 const decodificar = document.getElementById('decodificar');
+const codificar = document.getElementById('codificar')
 
 const textoFinal = document.getElementById('textoFinal');
 
 //Codificando
-document.getElementById('codificar').addEventListener('click', () => {
-
+codificar.addEventListener('click', () => {
     const textarea = document.getElementById('textoInicial').value; 
     let result =[];
     const chave = +document.getElementById('chave').value;
     let listaText = textarea.split('');
     console.log(chave)
-    
+    if (seleciona.value == 'cesar'){
+
+   
     for (let i=0; i< listaText.length; i++){
         if(listaText[i].charCodeAt() >=65 && listaText[i].charCodeAt() <= 90 ){
             let str = String.fromCharCode((((listaText[i].charCodeAt() - 65) + chave) % 26) + 65)
@@ -37,18 +40,26 @@ document.getElementById('codificar').addEventListener('click', () => {
     
     console.log(result)
     textoFinal.value = result.join('');
-    
+    }
+
+    else if (seleciona.value == 'base64'){ 
+        textoFinal.innerText = btoa(textarea)
+      }
+
     })
 
-//Decodificando
-document.getElementById('decodificar').addEventListener('click', () => {
 
+
+//Decodificando
+decodificar.addEventListener('click', () => {
     const textarea = document.getElementById('textoInicial').value; 
     let result =[];
     const chave = +document.getElementById('chave').value;
     let listaText = textarea.split('');
     console.log(chave)
     
+    if (seleciona.value == 'cesar'){
+
     for (let i=0; i< listaText.length; i++){
         if(listaText[i].charCodeAt() >=65 && listaText[i].charCodeAt() <= 90 ){
             let str = String.fromCharCode((((listaText[i].charCodeAt() - 90 )- chave) % 26) + 90)
@@ -69,22 +80,48 @@ document.getElementById('decodificar').addEventListener('click', () => {
     
     console.log(result)
     textoFinal.value = result.join('');
-    
+    }
+
+    else if (seleciona.value == 'base64'){ 
+        textoFinal.innerText = atob(textarea)
+      }
+
     })
 
 
 
-// //codificar base64
-// function cod64(){
-//     let base64 = btoa(receiveData)
-
-//     return base64
-// }
-// //decodificar base64
-// function decod64(){
-//     let base64 = atob(receiveData)
-
-//     return base64
-// }
 
 
+    // Verificadores para exibição
+
+    
+    let opCod = document.querySelector('#radioCodificar')
+    let opDecod = document.querySelector('#radioDecodificar')
+
+    opCod.addEventListener('click', () => {
+
+        if(opCod.value == 'cod'){
+            codificar.style.display = 'block'
+            decodificar.style.display = 'none'
+
+        }
+    })
+    
+    opDecod.addEventListener('click', () => {
+    
+        if(opDecod.value == 'decod'){
+            codificar.style.display = 'none'
+            decodificar.style.display = 'block'
+          
+        }
+    })
+
+    const divchave = document.querySelector('#incrementoCesar')
+    cifra.addEventListener('click', () => {
+        if (seleciona.value == 'cesar'){
+            divchave.style.display = 'block'
+        }
+        else if (seleciona.value == 'base64'){ 
+            divchave.style.display = 'none'
+        }
+    })
